49cc214d64ebec70819d34988d26adf3ed1fde25
EllaKaye <ella.kaye@gmail.com>
2025-02-25 11:49:14 +0000

    Add 2025 and EK solutions

---
+++ 2025/1_C/prime/prime_ek.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        min = get_int("Minimum: ");
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        max = get_int("Maximum: ");
+    }
+    while (min >= max);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(int number)
+{
+    if (number == 1)
+    {
+        return false;
+    
+    for (int i = 2; i < number; i++)
+    {
+        // if the number % i is 0 (i.e. false), the number is not prime
+        // in this case, !(number % i) is true
+        if (!(number % i))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
